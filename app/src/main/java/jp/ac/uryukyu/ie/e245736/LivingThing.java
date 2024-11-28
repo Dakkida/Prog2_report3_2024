0e091ab42937b3f2dd636440734907108b8e4039
Daiki Uehara <e245736@ie.u-ryukyu.ac.jp>
2024-11-28 20:08:05 +0900

    report4step2

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245736/LivingThing.java
@@ -0,0 +1,50 @@
+package jp.ac.uryukyu.ie.e245736;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead = false;
+
+    public LivingThing (String name, int hitPoint, int attack){
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+    }
+
+    public boolean isDead() {
+        return this.dead;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHitPoint() {
+        return this.hitPoint;
+    }
+
+    public void attack(LivingThing opponent) {
+        if (this.isDead() != true) {
+        int damage = (int)(Math.random() * attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+        opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if( hitPoint <= 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+
+    public void deadTrue(boolean judge) {
+        this.dead = true;
+    }
+
+    public void hitPointDec(int point) {
+        this.hitPoint = point;
+    }
+}
